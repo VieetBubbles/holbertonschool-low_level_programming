b2ad5d97a35ac16f0968b5cacf07d1c59a7fdf1a
VieetBubbles <537@holbertonschool.com>
2019-02-08 05:04:04 +0000

    Changed the for loop and putchar statements to be more readable

--- 0x01-variables_if_else_while/10-print_comb2.c
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -11,14 +11,14 @@ int main(void)
 	int i;
 	int second_Digit;
 
-	for (i = '0'; i <= '9'; i++)
+	for (i = 0; i < 10; i++)
 	{
-		for (second_Digit = '0'; second_Digit <= '9'; second_Digit++)
+		for (second_Digit = 0; second_Digit < 10; second_Digit++)
 		{
-			putchar(i);
-			putchar(second_Digit);
+			putchar(i + '0');
+			putchar(second_Digit + '0');
 
-			if (second_Digit == '9' && i == '9')
+			if (second_Digit == 9 && i == 9)
 			{
 				putchar('\n');
 			}
