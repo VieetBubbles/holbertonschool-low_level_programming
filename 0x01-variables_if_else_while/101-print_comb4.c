f60c8006226a968c6ca7acda82775e48f6933074
VieetBubbles <537@holbertonschool.com>
2019-02-09 05:52:07 +0000

    Created combination c program for numbers

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int first;
+	int second;
+	int third;
+
+	for (first = 0; first < 10; first++)
+	{
+		for (second = first + 1; second < 10; second++)
+		{
+			for (third = second + 1; third < 10; third++)
+			{
+				putchar(first + '0');
+				putchar(second + '0');
+				putchar(third + '0');
+
+				if (first != 7 || second != 8 || third != 9)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+				else
+				{
+                                        putchar('\n');
+                                }
+			}
+		}
+	}
+
+		return (0);
+}
---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int a;
+	int b;
+
+	a = 0;
+	while (a <= 98)
+	{
+		b = a + 1;
+		while (b <= 99)
+		{
+			putchar(a / 10 % 10 + '0');
+			putchar(a % 10 + '0');
+			putchar(' ');
+			putchar(b / 10 % 10 + '0');
+			putchar(b % 10 = '0');
+			if (a == 98 && b == 99)
+			{
+				putchar('\n');
+			}
+			else
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			b++;
+		}
+		a++;
+	}
+	return (0);
+}
