0622aaa3c6ce0e730824e73798a87184889d1100
VieetBubbles <537@holbertonschool.com>
2019-02-13 00:30:26 +0000

    C program that tells if a letter is true or false

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * _isalpha - function that checks for alphabetic character.
+ * @c: integer input
+ *
+ * Return: Always 0
+ */
+
+int _isalpha(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	else if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
