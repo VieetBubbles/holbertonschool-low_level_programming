565448938819bde13be6a022ce427cde96f31561
VieetBubbles <537@holbertonschool.com>
2019-02-13 03:38:05 +0000

    C program that returns the sum

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * add - function that adds two integers and returns the result.
+ * @a: the first integer
+ * @b: the second integer
+ *
+ * Return: the sum
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
