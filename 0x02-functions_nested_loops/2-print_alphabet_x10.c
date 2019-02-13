77fbc79204f8ffcf501cb826f7ee9597ab703a50
VieetBubbles <537@holbertonschool.com>
2019-02-13 00:26:18 +0000

    Fixed indenting error

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,19 +1,24 @@
 #include <stdio.h>
 #include "holberton.h"
 
+/**
+ * print_alphabet_x10 - function that prints 10 times the alphabet,
+ * in lowercase, followed by a new line.
+ *
+ * Return: nothing
+ */
+
 void print_alphabet_x10(void)
 {
 	int counter;
-        char i;
+	char i;
 
 	for (counter = 0; counter <= 9; counter++)
 	{
-		for(i = 'a'; i <= 'z'; i++)
+		for (i = 'a'; i <= 'z'; i++)
 		{
 			_putchar(i);
 		}
 		_putchar('\n');
 	}
-
-        return;
 }
