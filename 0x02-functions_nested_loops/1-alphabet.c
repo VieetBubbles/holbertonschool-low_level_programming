d42cc129bfc85ddb2d3bafb65436cb4738fc9a8b
VieetBubbles <537@holbertonschool.com>
2019-02-13 00:21:14 +0000

    Fixed spacing error and added comments

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,16 +1,21 @@
 #include <stdio.h>
 #include "holberton.h"
 
+/**
+ * print_alphabet - function that prints the alphabet, in lowercase,
+ * followed by a new line.
+ *
+ * Return: nothing
+ */
+
 void print_alphabet(void)
 {
 	char i;
 
-	for(i = 'a'; i <= 'z'; i++)
+	for (i = 'a'; i <= 'z'; i++)
 	{
 		_putchar(i);
 	}
 
 	_putchar('\n');
-
-	return;
 }
