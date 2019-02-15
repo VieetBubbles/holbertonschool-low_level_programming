189ddfe44a03249121fc921e873a0a85c7ab816f
VieetBubbles <537@holbertonschool.com>
2019-02-15 03:55:15 +0000

    Took out th unnecessary outer loop

--- 0x03-more_functions_nested_loops/6-print_line.c
+++ 0x03-more_functions_nested_loops/6-print_line.c
@@ -10,19 +10,13 @@
 void print_line(int n)
 {
 	int i;
-	int j;
 
-	for (i = n; i <= n; i++)
+	if (n > 0)
 	{
-		for (j = 0; j <= n; j++)
+		for (i = 0; i < n; i++)
 		{
-			if (n < 0)
-			{
-				_putchar('\n');
-			}
 			_putchar('_');
 		}
-		_putchar('\n');
 	}
-
+	_putchar('\n');
 }
