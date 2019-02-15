9e857ac8b5587974a8c2b3e28324976dfad2c670
VieetBubbles <537@holbertonschool.com>
2019-02-15 01:42:44 +0000

    C program that prints 0 - 9 and a newline

---
+++ 0x03-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * print_numbers - function that prints the numbers,
+ * from 0 to 9, followed by a new line
+ *
+ * Return: nothing
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	i = 0;
+	while (i < 10)
+	{
+		_putchar(i + '0');
+		i++;
+	}
+
+	_putchar('\n');
+}
