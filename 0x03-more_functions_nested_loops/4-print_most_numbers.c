15a372030fd4b773265bb4fbaaaa58fc034d6f8b
VieetBubbles <537@holbertonschool.com>
2019-02-15 01:51:00 +0000

    C program that prints 0-9 except 2 & 4

---
+++ 0x03-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * print_most_numbers - function that prints the numbers, from 0 to 9,
+ * followed by a new line. Except do not print 2 and 4
+ *
+ * Return: nothing
+ */
+
+void print_most_numbers(void)
+{
+	int i;
+
+	i = 0;
+	while (i <= 9)
+	{
+		if (i != 2 || i != 4)
+		{
+			_putchar(i + '0');
+		}
+		i++;
+	}
+	_putchar('\n');
+}
