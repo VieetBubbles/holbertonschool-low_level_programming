57815ddbd1ec5aff7da372e74ebe259782b54f6e
VieetBubbles <537@holbertonschool.com>
2019-02-15 04:25:58 +0000

    C program that prints #

---
+++ 0x03-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+
+/**
+ * print_square - function that prints a square, followed by a new line
+ * @size: integer
+ *
+ * Return: nothing
+ */
+
+void print_square(int size)
+{
+	int i;
+	int j;
+
+	if (size > 0)
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
