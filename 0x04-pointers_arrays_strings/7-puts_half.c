0b3b28bfb84dc0400986a510bc96680b10e4e090
VieetBubbles <537@holbertonschool.com>
2019-02-19 21:17:36 +0000

    Fixed betty spacing error

--- 0x04-pointers_arrays_strings/7-puts_half.c
+++ 0x04-pointers_arrays_strings/7-puts_half.c
@@ -16,9 +16,9 @@ void puts_half(char *str)
 
 	len = _strlen(str);
 
-	if(len % 2 == 0 || len % 2 != 0)
+	if (len % 2 == 0 || len % 2 != 0)
 	{
-		for (i = len / 2; i < len; i++)
+		for (i = len / 2; i <= len; i++)
 		{
 			_putchar(str[i]);
 		}
