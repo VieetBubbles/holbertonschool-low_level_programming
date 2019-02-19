e5b4a60f014a51d2b3e0165e07ab58c617fc1317
VieetBubbles <537@holbertonschool.com>
2019-02-19 21:24:46 +0000

    Rearranged my if statement to handle even and odd strings

--- 0x04-pointers_arrays_strings/7-puts_half.c
+++ 0x04-pointers_arrays_strings/7-puts_half.c
@@ -16,9 +16,16 @@ void puts_half(char *str)
 
 	len = _strlen(str);
 
-	if (len % 2 == 0 || len % 2 != 0)
+	if (len % 2 == 0)
 	{
-		for (i = len / 2; i <= len; i++)
+		for (i = len / 2; i < len; i++)
+		{
+			_putchar(str[i]);
+		}
+	}
+	else if (len % 2 != 0)
+	{
+		for (i = (len + 1) / 2; i < len; i++)
 		{
 			_putchar(str[i]);
 		}
