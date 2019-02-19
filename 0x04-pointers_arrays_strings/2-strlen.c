e63d608a56c4462ec228341afaa7a484765b18a9
VieetBubbles <537@holbertonschool.com>
2019-02-19 00:00:49 +0000

    C program that returns the string length

---
+++ 0x04-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * _strlen - function that returns the length of a string
+ * @s: pointer to a char
+ *
+ * Return: the string length
+ */
+
+int _strlen(char *s)
+{
+	int length;
+
+	length = 0;
+	while(s[length])
+	{
+		length++;
+	}
+	return (length);
+}
