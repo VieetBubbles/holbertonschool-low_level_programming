73066f7d5f24242131e2c8f0a3339c2cc5d9e0f8
VieetBubbles <537@holbertonschool.com>
2019-02-19 20:46:53 +0000

    Added the _strlen function at the bottom of the function

--- 0x04-pointers_arrays_strings/4-print_rev.c
+++ 0x04-pointers_arrays_strings/4-print_rev.c
@@ -21,3 +21,22 @@ void print_rev(char *s)
 	}
 	_putchar('\n');
 }
+
+/**
+ * _strlen - function that returns the length of a string
+ * @s: pointer to a char
+ *
+ * Return: the string length
+ */
+
+int _strlen(char *s)
+{
+	int length;
+
+	length = 0;
+	while (s[length])
+	{
+		length++;
+	}
+	return (length);
+}
