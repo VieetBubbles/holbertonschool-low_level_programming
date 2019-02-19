d47ee6e29e769d6cd3d684de29e9135182275dcd
VieetBubbles <537@holbertonschool.com>
2019-02-19 00:58:38 +0000

    C program that reverses a string

---
+++ 0x04-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,48 @@
+#include "holberton.h"
+
+/**
+ * rev_string - function that reverses a string
+ * @s: pointer to a char
+ *
+ * Return: nothing
+ */
+
+void rev_string(char *s)
+{
+
+	int len, i;
+	char *begin_ptr, *end_ptr, temp;
+
+	/* Get the length of the string */
+
+	len = _strlen(s);
+
+	/* Set the begin_ptr and end_ptr */
+	/* initially to start of string */
+
+	begin_ptr = s;
+	end_ptr = s;
+
+	/* Move the end_ptr to the last character */
+
+	for (i = 0; i < len - 1; i++)
+	{
+		end_ptr++;
+	}
+
+	/* Swap the char from start and end */
+	/* index using begin_ptr and end_ptr */
+
+	for (i = 0; i < len / 2; i++)
+	{
+
+		/* swap character */
+		temp = *end_ptr;
+		*end_ptr = *begin_ptr;
+		*begin_ptr = temp;
+
+		/* update pointers positions */
+		begin_ptr++;
+		end_ptr--;
+	}
+}
