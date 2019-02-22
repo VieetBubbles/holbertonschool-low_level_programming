96f9e36a63f0f84510c974edc0f2b2ca0e042a91
VieetBubbles <537@holbertonschool.com>
2019-02-22 07:09:16 +0000

    C program that makes a correct sentence with capital words

---
+++ 0x05-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,37 @@
+#include "holberton.h"
+
+/**
+ * cap_string - function that capitalizes all words of a string
+ * @str: a pointer to a char string
+ *
+ * Return: a string
+ */
+
+char *cap_string(char *str)
+{
+	int i, j;
+	char seperators[13] = {' ', '\t', '\n', ',', ';', '.', '!', '?', '"',
+			     '(', ')', '{', '}'};
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (i == 0)
+		{
+			if (str[i] <= 'a' && str[i] >= 'z')
+			{
+				str[i] = str[i] - 32;
+			}
+		}
+		for (j = 0; j < 13; j++)
+		{
+			if (str[i] == seperators[j])
+			{
+				if (str[i + 1] >= 'a' && seperators[j + 1] <= 'z')
+				{
+					str[i + 1] = str[i + 1] - 32;
+				}
+			}
+		}
+	}
+	return (str);
+}
