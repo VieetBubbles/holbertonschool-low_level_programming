6e54800dde981fc287aba6d8fca6e933d6f38253
VieetBubbles <537@holbertonschool.com>
2019-02-22 08:41:56 +0000

    Reduced the statements length

--- 0x05-pointers_arrays_strings/6-cap_string.c
+++ 0x05-pointers_arrays_strings/6-cap_string.c
@@ -10,8 +10,8 @@
 char *cap_string(char *str)
 {
 	int i, j;
-	char seperators[13] = {' ', '\t', '\n', ',', ';', '.', '!', '?', '"',
-			     '(', ')', '{', '}'};
+	char sep[13] = {' ', '\t', '\n', ',', ';', '.', '!', '?', '"',
+			       '(', ')', '{', '}'};
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
@@ -19,16 +19,16 @@ char *cap_string(char *str)
 		{
 			if (str[i] <= 'a' && str[i] >= 'z')
 			{
-				str[i] = str[i] - 32;
+				str[i] -= 32;
 			}
 		}
 		for (j = 0; j < 13; j++)
 		{
-			if (str[i] == seperators[j])
+			if (str[i] == sep[j])
 			{
-				if (str[i + 1] >= 'a' && seperators[i + 1] <= 'z')
+				if (str[i + 1] >= 'a' && sep[i + 1] <= 'z')
 				{
-					str[i + 1] = str[i + 1] - 32;
+					str[i + 1] -= 32;
 				}
 			}
 		}
