92f4154cce8b25a2fd48eb3c228771f73f29e0eb
VieetBubbles <537@holbertonschool.com>
2019-02-23 02:26:23 +0000

    Fixed the array condition by replacing sep[] with str[]

--- 0x05-pointers_arrays_strings/6-cap_string.c
+++ 0x05-pointers_arrays_strings/6-cap_string.c
@@ -28,7 +28,7 @@ char *cap_string(char *str)
 		{
 			if (str[i] == sep[j])
 			{
-				if (str[i + 1] >= 'a' && sep[i + 1] <= 'z')
+				if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
 				{
 					str[i + 1] -= 32;
 				}
