841602f2d1a83bb95fcdb33ecdff0f61fee959ec
VieetBubbles <537@holbertonschool.com>
2019-02-25 23:28:57 +0000

    Placed previous if statement inside the while loop

--- 0x06-pointers_arrays_strings/5-strstr.c
+++ 0x06-pointers_arrays_strings/5-strstr.c
@@ -18,11 +18,13 @@ char *_strstr(char *haystack, char *needle)
 		{
 			return (haystack);
 		}
+
+		if (*needle == '\0')
+		{
+			return (haystack);
+		}
+
 		haystack++;
-	}
-	if (*needle == '\0')
-	{
-		return (haystack);
 	}
 
 	return (NULL);
