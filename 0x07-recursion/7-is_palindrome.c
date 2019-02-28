217faa142499eeed50b4ed761ad2a2d9b6ef7634
VieetBubbles <537@holbertonschool.com>
2019-02-28 06:00:36 +0000

    C program that checks if a string is a palindrome

---
+++ 0x07-recursion/7-is_palindrome.c
@@ -0,0 +1,77 @@
+#include "holberton.h"
+int _strLen(char *str);
+int check_ifPalindrome(char str[], int s, int e);
+
+/**
+ * is_palindrome - function that returns 1 if a string is a
+ * palindrome and 0 if not
+ * @s: the string
+ *
+ * Return: 1 if the string is a palindrome or 0 if not
+ */
+
+int is_palindrome(char *s)
+{
+	int len;
+
+	len = _strLen(s) - 1;
+
+	/* An empty string is considered as palindrome */
+	if (len == 0)
+	{
+		return (1);
+	}
+	return (check_ifPalindrome(s, 0, len));
+}
+
+/**
+ * _strLen - function that finds the length of a string
+ * @str: the string
+ *
+ * Return: the length of the string
+ */
+
+int _strLen(char *str)
+{
+	int length;
+
+	length = 0;
+	while (str[length])
+	{
+		length++;
+	}
+	return (length);
+}
+
+/**
+ * check_ifPalindrome - function that checks to see if the
+ * string is a palindrome
+ * @str: the string
+ * @s: the first letter of the string/array - at index 0
+ * @e: the last leeter of the string/array - at last index
+ *
+ * Return: 1 if the string is a palindorme or 0 if not
+ */
+
+int check_ifPalindrome(char str[], int s, int e)
+{
+	/* if there is only 1 character */
+	if (str[s] == str[e])
+	{
+		return (1);
+	}
+
+	/* if 1st and last characters do not match */
+	else if (str[s] != str[e])
+	{
+		return (0);
+	}
+
+/* If there are more than two characters, check if */
+/* middle substring is also palindrome or not. */
+	else if (s < e + 1)
+	{
+		return (check_ifPalindrome(str, s + 1, e - 1));
+	}
+	return (1);
+}
