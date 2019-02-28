acf9f819e2042c84bdaeeead6cc8ce77ae509d5b
VieetBubbles <537@holbertonschool.com>
2019-02-28 01:14:03 +0000

    C program that uses recursion to print a string

---
+++ 0x07-recursion/0-puts_recursion.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * _puts_recursion - function that prints a string, followed by a new line
+ * @s: a pointer to a char/string
+ *
+ * Return: nothing
+ */
+
+void _puts_recursion(char *s)
+{
+	_putchar(*s++);
+
+	if (!*s)
+	{
+		_putchar('\n');
+	}
+	if (*s)
+	{
+		(_puts_recursion (s));
+	}
+}
