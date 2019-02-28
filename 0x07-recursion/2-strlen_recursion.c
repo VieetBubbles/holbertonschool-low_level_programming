e6da6f4c419c7b123ee0f7cb7c7b16b210f53f8c
VieetBubbles <537@holbertonschool.com>
2019-02-28 02:01:01 +0000

    C program that gets string length with recursion

---
+++ 0x07-recursion/2-strlen_recursion.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * _strlen_recursion - function that returns the length of a string
+ * @s: the string
+ *
+ * Return: the string length
+ */
+
+int _strlen_recursion(char *s)
+{
+	/* If we reach the end of the string */
+	if (!*s)
+	{
+		return(0);
+	}
+	else
+	{
+		return (1 + _strlen_recursion(s + 1));
+	}
+}
