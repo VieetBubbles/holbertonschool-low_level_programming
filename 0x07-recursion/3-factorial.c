1411e1caeca6c4122562f5b749e2019f4c429aa0
VieetBubbles <537@holbertonschool.com>
2019-02-28 02:10:02 +0000

    C program that prints factorials

---
+++ 0x07-recursion/3-factorial.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * factorial - function that returns the factorial of a given number
+ * @n: an integer
+ *
+ * Return: the factorial result
+ */
+
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	if (n == 0)
+	{
+		return (1);
+	}
+	return (n * factorial(n - 1));
+}
