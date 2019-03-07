7629304eca04b5c4e310e2048307e8706fc36002
VieetBubbles <537@holbertonschool.com>
2019-03-07 21:58:33 +0000

    Removed the sizeof function from the malloc declaration

--- 0x0B-more_malloc_free/0-malloc_checked.c
+++ 0x0B-more_malloc_free/0-malloc_checked.c
@@ -6,11 +6,12 @@
  *
  * Return: a pointer to the allocated memory and if it fails exit 98
  */
+
 void *malloc_checked(unsigned int b)
 {
 	void *ptr;
 
-	ptr = malloc(sizeof(b) * b);
+	ptr = malloc(b);
 
 	if (!ptr)
 	{
