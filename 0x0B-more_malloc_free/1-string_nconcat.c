eb84380c9a47f2cf60571a23a62c5eae39ced999
VieetBubbles <537@holbertonschool.com>
2019-03-07 22:58:31 +0000

    Created a if statment to handle the n and strleng2

--- 0x0B-more_malloc_free/1-string_nconcat.c
+++ 0x0B-more_malloc_free/1-string_nconcat.c
@@ -27,25 +27,25 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	len2 = _strlen(s2);
 
 	if (n > len2)
-	{
 		ptr = malloc(sizeof(char) * (len1 + len2) + 1);
+	else
+		ptr = malloc(sizeof(char) * (len1 + n) + 1);
+
+	if (ptr == 0)
+		return (NULL);
+
+	for (i = 0; i < len1; i++)
+		ptr[i] = s1[i];
+
+	if (n < len2)
+	{
+		for (j = 0; j < n; j++, i++)
+			ptr[i] = s2[j];
 	}
 	else
 	{
-		ptr = malloc(sizeof(char) * (len1 + n) + 1);
-	}
-	if (ptr == 0)
-	{
-		return (NULL);
-	}
-
-	for (i = 0; i < len1; i++)
-	{
-		ptr[i] = s1[i];
-	}
-	for (j = 0; j < n; j++, i++)
-	{
-		ptr[i] = s2[j];
+		for (j = 0; j < len2; j++, i++)
+			ptr[i] = s2[j];
 	}
 	ptr[i] = '\0';
 	return (ptr);
