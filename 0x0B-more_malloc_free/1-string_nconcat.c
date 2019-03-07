3d1ade4ea6a5cc579368ba720f8d4054e919cb6d
VieetBubbles <537@holbertonschool.com>
2019-03-07 22:51:36 +0000

    Changed the >= to >

--- 0x0B-more_malloc_free/1-string_nconcat.c
+++ 0x0B-more_malloc_free/1-string_nconcat.c
@@ -26,7 +26,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	len1 = _strlen(s1);
 	len2 = _strlen(s2);
 
-	if (n >= len2)
+	if (n > len2)
 	{
 		ptr = malloc(sizeof(char) * (len1 + len2) + 1);
 	}
