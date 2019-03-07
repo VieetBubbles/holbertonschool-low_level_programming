3759cb209f60cb9dfdb33334b8fbd868fc9ac8be
VieetBubbles <537@holbertonschool.com>
2019-03-07 21:55:54 +0000

    C program that allocates memory

---
+++ 0x0B-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * malloc_checked - function that allocates memory using malloc
+ * @b: the size of the memory to be allocatd
+ *
+ * Return: a pointer to the allocated memory and if it fails exit 98
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *ptr;
+
+	ptr = malloc(sizeof(b) * b);
+
+	if (!ptr)
+	{
+		exit(98);
+	}
+
+	return (ptr);
+}
