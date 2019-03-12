225c1e61a31899a10f77a7b9648bc17864f07a21
VieetBubbles <537@holbertonschool.com>
2019-03-12 02:54:53 +0000

    Created C program that initalizes a struct called dog

---
+++ 0x0D-structures_typedef/1-init_dog.c
@@ -0,0 +1,25 @@
+#include "dog.h"
+
+/**
+ * init_dog - function that initialize a variable of type struct dog
+ * @d: a pointer to the struct named dog
+ * @name: the name of the dog
+ * @age: the age of the dog
+ * @owner: th owner of the dog
+ *
+ * Return: nothing
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+	else
+	{
+		return;
+	}
+}
