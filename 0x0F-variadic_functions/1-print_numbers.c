698f28b8bd5f26649ed3056da536bcd757275eb0
VieetBubbles <537@holbertonschool.com>
2019-03-15 20:55:54 +0000

    C program that prints out the integers using variadic functions

---
+++ 0x0F-variadic_functions/1-print_numbers.c
@@ -0,0 +1,34 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - function that prints numbers, followed by a new line
+ * @separator: the comma and space that is placed in between the numbers
+ * @n: the number of arguments to print out
+ *
+ * Return: nothing
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list list;
+	unsigned int i;
+
+	va_start(list, n);
+
+	for (i = 0; i < n; i++)
+	{
+		if (separator)
+		{
+			printf("%d", va_arg(list, int));
+			if (i < n - 1)
+			{
+				printf("%s", separator);
+			}
+		}
+		else
+		{
+			return;
+		}
+	}
+	printf("\n");
+	va_end(list);
+}
