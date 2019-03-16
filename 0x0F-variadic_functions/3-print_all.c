b781d688dd4f8ac76da52f9aa00c7b36c81fe815
VieetBubbles <537@holbertonschool.com>
2019-03-16 01:56:24 +0000

    C program to print all arguments

---
+++ 0x0F-variadic_functions/3-print_all.c
@@ -0,0 +1,98 @@
+#include "variadic_functions.h"
+
+/**
+ * _print_char - function that prints a char
+ * @ap: the argument to print
+ *
+ * Return: nothing
+ */
+
+void _print_char(va_list ap)
+{
+	printf("%c", va_arg(args, int));
+}
+
+/**
+ * _print_int - function that prints an integer
+ * @ap: the argument to print
+ *
+ * Return: nothing
+ */
+
+void _print_int(va_list ap)
+{
+	printf("%i", va_arg(args, int));
+}
+
+/**
+ * _print_float - function that prints a float
+ * @ap: the argument to print
+ *
+ * Return: nothing
+ */
+
+void _print_float(va_list ap)
+{
+	printf("%f", va_arg(args, double));
+}
+
+/**
+ * _print_string - function that prints a string
+ * @ap: the argument to print out
+ *
+ * Return: nothing
+ */
+
+void _print_string(va_list ap)
+{
+	char *s;
+
+	s = va_arg(args, char *);
+
+	if (!s)
+	{
+		printf("(nil)");
+	}
+	printf("%s", s);
+}
+
+/**
+ * print_all - function that prints anything
+ * @format: a list of types of arguments passed to the function
+ *
+ * Return: nothing
+ */
+
+void print_all(const char * const format, ...)
+{
+	po_t array[] = {
+		{"c", _print_char},
+		{"i", _print_int},
+		{"f", _print_float},
+		{"s", _print_string},
+		{NULL, NULL},
+	};
+	int i = 0, j;
+	char *separator = "";
+	va_list ap;
+
+	va_start(args, format);
+
+	while (format[i])
+	{
+		j = 0;
+		while (array[j].po)
+		{
+			if (format[i] == *array[j].po)
+			{
+				printf("%s", separator);
+				array[j].f(args);
+				separator = ", ";
+			}
+			j++;
+		}
+		i++;
+	}
+	printf("\n");
+	va_end(args);
+}
