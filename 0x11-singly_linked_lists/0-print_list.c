5949fb6702eafb47acfce90cae007762a30d2854
VieetBubbles <537@holbertonschool.com>
2019-03-21 23:32:21 +0000

    C program that prints th elements in a linked list

---
+++ 0x11-singly_linked_lists/0-print_list.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+
+/**
+ * print_list - function that prints all the elements of a list_t list
+ * @h: the head/beginning of the linked list
+ *
+ * Return: the number of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t count;
+
+	count = 0;
+
+	while (h)
+	{
+		if (!h->str)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+			printf("[%i] %s\n", h->len, h->str);
+		}
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
