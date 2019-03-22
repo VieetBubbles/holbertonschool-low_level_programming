3332e47f3addf3e98aeb3106bd2afd189fc1033e
VieetBubbles <537@holbertonschool.com>
2019-03-22 03:56:07 +0000

    Assembly code that prints hello holberton

---
+++ 0x11-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,16 @@
+	section .data
+
+fmt:	db "%s", 10, 0
+msg:	db "Hello Holberton", 0
+
+	section .text
+	extern printf
+	global _start
+_start:
+	mov exd, msg1
+	mov esi, 1		;used website https://montcs.bloomu.edu/~bobmon/Code/Asm.and.C/hello-asms.html
+	mov edi, fmt
+	mov eax, 0
+	call printf
+	mov eax, 0
+	mov ebx, 0
