1dc06fedec51529634c074917d42e80aab5b960b
VieetBubbles <537@holbertonschool.com>
2019-03-22 03:04:00 +0000

    C program that frees a linked lis

---
+++ 0x11-singly_linked_lists/4-free_list.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_list - function that frees a list_t list
+ * @head: a pointer to the beginnin go fthe linked list
+ *
+ * Return: nothing
+ */
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+	while (head)
+	{
+		temp = head->next;
+		free(head->str);
+		free(head);
+		head = temp;
+	}
+	head = NULL;
+}
