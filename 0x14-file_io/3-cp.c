39ce5207c4bbbbea6b9663162f35113134bd7885
VieetBubbles <537@holbertonschool.com>
2019-04-03 02:11:59 +0000

    fixed the parartheses issue inside the while loop causing it not to compile

--- 0x14-file_io/3-cp.c
+++ 0x14-file_io/3-cp.c
@@ -26,7 +26,7 @@ int main(int argc, char **argv)
 	}
 
 	file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	while (r = read(file_from, buffer, 1024) > 0)
+	while ((r = read(file_from, buffer, 1024) > 0))
 	{
 		if (file_to < 0 || write(file_to, buffer, r) != r)
 			dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", argv[2]);
