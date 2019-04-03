7a1b79e3b18c706a3c64452cfafada7d8b2663b6
VieetBubbles <537@holbertonschool.com>
2019-04-03 03:18:50 +0000

    Shortened the number of lines by sticking exit 97, 98, and 99 to the end of dprintf

--- 0x14-file_io/3-cp.c
+++ 0x14-file_io/3-cp.c
@@ -15,28 +15,24 @@ int main(int argc, char **argv)
 
 	if (argc != 3)
 	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
 	}
 	f_from = open(argv[1], O_RDONLY);
 	if (f_from < 0)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98);
 	}
 	f_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	while ((r = read(f_from, buffer, 1024)) > 0)
 	{
 		if (f_to < 0 || (write(f_to, buffer, r) != r))
 		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
 		}
-		exit(99);
 	}
 	if (r < 0)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98);
 	}
 	closef = close(f_from);
 	if (closef < 0)
